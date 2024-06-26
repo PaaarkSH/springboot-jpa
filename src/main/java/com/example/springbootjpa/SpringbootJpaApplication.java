23f3237dce207363e1b77652b4140538e19169a0
psh950 <psh950@naver.com>
2024-06-27 00:25:43 +0900

    하던거

--- src/main/java/com/example/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/example/springbootjpa/SpringbootJpaApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootJpaApplication {
 
     public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.setData("hello");
+        System.out.println("hello: " + hello.getData());
+
         SpringApplication.run(SpringbootJpaApplication.class, args);
     }
 
