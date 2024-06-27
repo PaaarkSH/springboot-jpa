33e08b76a1e73114950746c46f6831129804cafc
psh950 <psh950@naver.com>
2024-06-28 00:22:04 +0900

    하던거

---
+++ src/test/java/com/example/springbootjpa/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.example.springbootjpa;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    @Transactional
+    void testMember () throws Exception{
+        // give
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
