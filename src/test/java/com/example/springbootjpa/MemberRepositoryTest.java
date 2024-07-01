2f9699b21114f0be2593736bd1687c2b7a6128ae
psh950 <psh950@naver.com>
2024-07-02 00:23:54 +0900

    jpa db 동작 확인

--- src/test/java/com/example/springbootjpa/MemberRepositoryTest.java
+++ src/test/java/com/example/springbootjpa/MemberRepositoryTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -16,6 +17,7 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(false)
     void testMember () throws Exception{
         // give
         Member member = new Member();
@@ -25,9 +27,9 @@ class MemberRepositoryTest {
         Long savedId = memberRepository.save(member);
         Member findMember = memberRepository.find(savedId);
 
-
         // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
