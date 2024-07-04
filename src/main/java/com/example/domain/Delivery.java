901b939c1c1cb6f05081753250c57acffa6cfafd
psh950 <psh950@naver.com>
2024-07-05 00:01:17 +0900

    하던거

--- src/main/java/com/example/domain/Delivery.java
+++ src/main/java/com/example/domain/Delivery.java
@@ -1,4 +1,21 @@
 package com.example.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
 public class Delivery {
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.ORDINAL)  // 숫자로 들어감
+    private DeliveryStatus status;  // ready, comp
 }
--- src/main/java/com/example/domain/OrderItem.java
+++ src/main/java/com/example/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package com.example.domain;
 
+import com.example.domain.item.Item;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +13,8 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
+    @ManyToOne()
+    @JoinColumn(name = "item_id")
     private Item item;
 
     @ManyToOne
