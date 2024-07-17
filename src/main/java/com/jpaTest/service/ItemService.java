7789613edd8ca0b7ad24ac01cbb3b7d7ab36ac5a
psh950 <psh950@naver.com>
2024-07-17 23:53:30 +0900

    상품 서비스 개발

---
+++ src/main/java/com/jpaTest/service/ItemService.java
@@ -0,0 +1,29 @@
+package com.jpaTest.service;
+
+import com.jpaTest.domain.item.Item;
+import com.jpaTest.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    @Transactional  // 이부분은 readOnly 가 아님
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long id) {
+        return itemRepository.findOne(id);
+    }
+}
