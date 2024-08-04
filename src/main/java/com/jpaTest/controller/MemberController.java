d67391d51043541094e4d8f9ec61cdce1573216c
psh950 <psh950@naver.com>
2024-08-04 22:55:53 +0900

    회원가입

--- src/main/java/com/jpaTest/controller/MemberController.java
+++ src/main/java/com/jpaTest/controller/MemberController.java
@@ -1,10 +1,15 @@
 package com.jpaTest.controller;
 
+import com.jpaTest.domain.Address;
+import com.jpaTest.domain.Member;
 import com.jpaTest.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,7 +18,23 @@ public class MemberController {
 
     @GetMapping("/members/new")
     public String newMember(Model model){
-        model.addAttribute("member", new MemberForm());
+        model.addAttribute("member", new MemberForm());  // 해당 form 에 접근 할 수 있도록 정보를 넘김
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new ")
+    public String create(@Valid MemberForm form, BindingResult result){
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipCode());
+        Member member = new Member();
+        member.setUsername(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
     }
 }
