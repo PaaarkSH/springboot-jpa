d78dd2480824596c7415d1e1c8a8cd2be4acbed2
psh950 <psh950@naver.com>
2024-08-13 00:03:45 +0900

    하던거

---
+++ src/main/java/com/jpaTest/controller/OrderController.java
@@ -0,0 +1,36 @@
+package com.jpaTest.controller;
+
+import com.jpaTest.domain.Member;
+import com.jpaTest.domain.item.Item;
+import com.jpaTest.service.ItemService;
+import com.jpaTest.service.MemberService;
+import com.jpaTest.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    // RequiredArgsConstructor 어노테이션이 생성자 초기화 대 해줘서 변수만 있으면 되는걸 기억못함
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+
+    }
+}
